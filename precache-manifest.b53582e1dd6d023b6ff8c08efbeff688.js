5863b72cbf415b664636b20d69f5858a2d2601ef
Jiacheng Zhang <jiachengzhang1@email.arizona.edu>
2020-08-16 17:03:21 -0700

    Updates

---
+++ precache-manifest.b53582e1dd6d023b6ff8c08efbeff688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9994dafbc4a52dcda9b54e8ceee2a637",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "aad2c38ada411f30246a",
+    "url": "/portfolio-react/static/css/main.d9443dc5.chunk.css"
+  },
+  {
+    "revision": "a11129929e7f9af53242",
+    "url": "/portfolio-react/static/js/2.27fe0091.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-react/static/js/2.27fe0091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad2c38ada411f30246a",
+    "url": "/portfolio-react/static/js/main.66507c28.chunk.js"
+  },
+  {
+    "revision": "0b49612531ede288327f",
+    "url": "/portfolio-react/static/js/runtime-main.40d860e6.js"
+  }
+]);
